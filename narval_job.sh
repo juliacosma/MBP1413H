4f0146c6ccb9e243ce81360bcbd761fc5543bae6
Julia Cosma <jules.cosma@gmail.com>
2026-04-01 10:51:03 -0400

    formatting for HPC + out log
    converted notebook to python file, create job setup file, out put log from first run

---
+++ narval_job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=def-mchiew           # VERY IMPORTANT: Replace with your PI's account name
+#SBATCH --gpus-per-node=a100:1         # Request 1 A100 GPU (Narval's standard powerhouse)
+#SBATCH --cpus-per-task=8              # Matches the NUM_WORKERS = 8 in your code
+#SBATCH --mem=32G                      # Plenty of RAM for loading the MRI data
+#SBATCH --time=04:00:00                # Max runtime (4 hours should be plenty for 60 epochs)
+#SBATCH --job-name=tms_fmri_train
+#SBATCH --output=%x-%j.out             # Saves all your print statements to a log file
+
+# 1. load Narval modules
+module load StdEnv/2023
+module load python/3.11
+module load arrow/15.0.1
+
+# 2. create + activate venv
+virtualenv --no-download ~/tms_env
+source ~/tms_env/bin/activate
+
+# 3. install python packages
+pip install --no-index --upgrade pip
+pip install monai s3fs nibabel pandas seaborn scikit-learn tqdm scipy
+
+# 4. run code!
+python tms-fmri-classifier-narval.py
